df8f8ca8560e8b079e66ba95a5c1ab8b2dbba8c9
Brittney Haselmayer <brittneyhaselmayer@yahoo.com>
2019-10-11 00:11:51 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
   
-  for array.length.times do
+  array.length.times do 
     array.include?(value_to_find)
     return array.index(value_to_find) 
   
